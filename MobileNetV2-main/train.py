444034892e294f0d68546e24e3d814e7772691e4
uvd174 <st075915@student.spbu.ru>
2022-10-08 14:15:16 +0300

    Implemented CarNumbersDataset class

--- MobileNetV2-main/train.py
+++ MobileNetV2-main/train.py
@@ -1,24 +1,38 @@
+import os.path
+from typing import Tuple, Callable, Optional
+from torch.utils.data import Dataset
+
+import cv2
 import torch
-import torchsummary
+import pandas as pd
 
 
-class CarNumbersDataset(torch.utils.data.Dataset):
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
+class CarNumbersDataset(Dataset):
+    def __init__(self, data_path: str, transform: Optional[Callable] = None):
+        """
+        :param data_path: path to the .csv file with image paths and labels
+        """
+        self.base_path = os.path.dirname(data_path)
+        self.transform = transform
+        self.data = pd.read_csv(data_path, header=0)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.data)
 
-    def __getitem__(self, index):
-        return self.data[index], self.labels[index]
+    def __getitem__(self, index) -> Tuple[torch.FloatTensor, int]:
+        image_path, label = self.data.iloc[index]
+
+        image = cv2.imread(os.path.join(self.base_path, image_path))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if self.transform is not None:
+            image = self.transform(image=image)['image']
+
+        image = torch.from_numpy(image).float()
+
+        return image, int(label)
 
 
 if __name__ == '__main__':
     model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
-    model.to('cuda')
-    torchsummary.summary(model, (3, 224, 224))
-    print(model)
-    model.classifier[1] = torch.nn.Linear(in_features=1280, out_features=2, bias=True).to('cuda')
-    torchsummary.summary(model, (3, 224, 224))
-    print(model)
+    dataset = CarNumbersDataset('../dataset/classification/train.csv')
